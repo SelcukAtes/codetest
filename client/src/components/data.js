289e8668ddf70537b77836154b9db60d40f2e6f2
SelcukAtes <sselcukates@gmail.com>
2019-02-21 23:36:02 -0800

    created new fighter.js file to render botton and inside the data handleNext and handlePrevious function added now user can able to swicht back and forward to fighters"

--- client/src/components/data.js
+++ client/src/components/data.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-
+import Fighter from './fighter';
 class Data extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			fighters: null
+			fighters: null,
+			current: null
 		};
 
 		this.getData = this.getData.bind(this);
+		this.handlePrevious = this.handlePrevious.bind(this);
+		this.handleNext = this.handleNext.bind(this);
 	}
 
 	componentDidMount() {
@@ -26,11 +29,33 @@ class Data extends Component {
 			.catch((err) => console.log(err));
 	}
 
+	handlePrevious() {
+		// prev decrements the state, however if less than 0 we set to length
+		let index = this.state.current - 1;
+		if (index < 0) index = this.state.fighters.length - 1;
+		this.setState({ current: index });
+	}
+
+	handleNext() {
+		// next increments the state, however if greater than length we set to 0
+		let index = this.state.current + 1;
+		if (index >= this.state.fighters.length) index = 0;
+		this.setState({ current: index });
+	}
+
 	render() {
-		if (!this.state.fighters) {
-			return null;
-		}
-		return <div>{this.state.fighters.map((fighter) => <div>{fighter}</div>)}</div>;
+		// selected refers to the current fighter in the fighters list
+		let selected;
+		// once the state is populated with the fighters list, then we designate the selected fighter
+		if (this.state.fighters) selected = this.state.fighters[this.state.current];
+
+		return (
+			<div>
+				{selected && (
+					<Fighter selected={selected} handlePrevious={this.handlePrevious} handleNext={this.handleNext} />
+				)}
+			</div>
+		);
 	}
 }
 
---
+++ client/src/components/fighter.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Fighter = (props) => {
+	return <div>Will render all botton functinality in here</div>;
+};
+
+export default Fighter;
