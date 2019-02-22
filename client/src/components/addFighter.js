04d417db8fd5007fe905622b66971946d8bd271f
SelcukAtes <sselcukates@gmail.com>
2019-02-22 00:35:15 -0800

    added file upload from the user using formData object

--- client/src/components/addFighter.js
+++ client/src/components/addFighter.js
@@ -4,11 +4,31 @@ import axios from 'axios';
 class AddFighter extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			selectedFile: null
+		};
 	}
 
+	fileSelectedHandler = (event) => {
+		this.setState({ selectedFile: event.target.files[0] });
+	};
+
+	fileUploadHandler = (e) => {
+		e.preventDefault();
+		const fd = new FormData();
+		fd.append('img', this.state.selectedFile, this.state.selectedFile.name);
+		axios.post('/createUser', fd).then((res) => {
+			console.log(res);
+		});
+	};
+
 	render() {
-		return <form>THIS WILL BE FORM ELELEMENT</form>;
+		return (
+			<form onSubmit={this.fileUploadHandler}>
+				<input type="file" onChange={this.fileSelectedHandler} />
+				<button type="submit">Upload</button>
+			</form>
+		);
 	}
 }
 
