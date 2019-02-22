b34aacc962cb1b55c9353d71bc85b0267f95116f
SelcukAtes <sselcukates@gmail.com>
2019-02-22 00:03:34 -0800

    handleDelete renderent in fighter.js passed as a props

--- client/src/components/fighter.js
+++ client/src/components/fighter.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Fighter = (props) => {
 	const { firstName, lastName, img, _id } = props.selected;
-	const { handlePrevious, handleNext } = props;
+	const { handlePrevious, handleNext, handleDelete } = props;
 
 	return (
 		<div>
@@ -11,6 +11,9 @@ const Fighter = (props) => {
 			<img src={img} />
 			<button onClick={handlePrevious}>Previous</button>
 			<button onClick={handleNext}>Next</button>
+			<button value={`${_id}`} onClick={(e) => handleDelete(e)}>
+				Delete
+			</button>
 		</div>
 	);
 };
