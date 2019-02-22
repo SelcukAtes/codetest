871ffd7874e2b8e990f7f621d2938eb4ef03836b
SelcukAtes <sselcukates@gmail.com>
2019-02-22 00:50:04 -0800

    in the form data append userName lastName to form data when uploading the form data

--- client/src/components/addFighter.js
+++ client/src/components/addFighter.js
@@ -5,7 +5,9 @@ class AddFighter extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			selectedFile: null
+			selectedFile: null,
+			firstName: '',
+			lastName: ''
 		};
 	}
 
@@ -13,18 +15,31 @@ class AddFighter extends Component {
 		this.setState({ selectedFile: event.target.files[0] });
 	};
 
+	// this file uploads and grabs the user firstName and lastName
 	fileUploadHandler = (e) => {
 		e.preventDefault();
 		const fd = new FormData();
 		fd.append('img', this.state.selectedFile, this.state.selectedFile.name);
+		fd.append('firstName', this.state.firstName);
+		fd.append('lastName', this.state.lastName);
 		axios.post('/createUser', fd).then((res) => {
 			console.log(res);
 		});
 	};
 
+	handleFirstChange = (e) => {
+		this.setState({ firstName: e.target.value });
+	};
+
+	handleLastChange = (e) => {
+		this.setState({ lastName: e.target.value });
+	};
+
 	render() {
 		return (
 			<form onSubmit={this.fileUploadHandler}>
+				<input type="text" value={this.state.firstName} onChange={(e) => this.handleFirstChange(e)} />
+				<input type="text" value={this.state.lastName} onChange={(e) => this.handleLastChange(e)} />
 				<input type="file" onChange={this.fileSelectedHandler} />
 				<button type="submit">Upload</button>
 			</form>
