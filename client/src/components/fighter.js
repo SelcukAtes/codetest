7b2b6a729a0fd1b2ba6e1d150a77d99a270ae874
SelcukAtes <sselcukates@gmail.com>
2019-02-22 01:18:55 -0800

    rendered features of the fighters I forgot to rendered earlier now its completed

--- client/src/components/fighter.js
+++ client/src/components/fighter.js
@@ -2,13 +2,14 @@ import React from 'react';
 import AddFighter from './addFighter';
 
 const Fighter = (props) => {
-	const { firstName, lastName, img, _id } = props.selected;
+	const { firstName, lastName, features, img, _id } = props.selected;
 	const { handlePrevious, handleNext, handleDelete } = props;
 
 	return (
 		<div>
 			<p>{firstName}</p>
 			<p>{lastName}</p>
+			<p>{features}</p>
 			<img src={img} />
 			<button onClick={handlePrevious}>Previous</button>
 			<button onClick={handleNext}>Next</button>
